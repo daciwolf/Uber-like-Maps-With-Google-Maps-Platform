fb7d6e5a742ce9b54c458257f4c0ab32eeb6ab3b
daciwolf <davidculciar@gmail.com>
2024-04-20 04:36:36 -0700

    apikey

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("YOUR_API_KEY_HERE")
+    GMSServices.provideAPIKey("AIzaSyAyIr3ZBmZUMAi9kZML91y3JlLc8sFYmPw")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
